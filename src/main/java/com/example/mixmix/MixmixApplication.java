b09f3d8de04b923ec4dd94b34ecfca53a7ba8441
inhooo00 <chldlsgh0987@naver.com>
2024-11-18 00:10:01 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/example/mixmix/MixmixApplication.java
@@ -0,0 +1,13 @@
+package com.example.mixmix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixmixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MixmixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mixmix/MixmixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mixmix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MixmixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
